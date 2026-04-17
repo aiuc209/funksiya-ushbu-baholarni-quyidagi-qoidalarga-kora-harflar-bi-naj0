bb6b9bb703d49dcf57a1610d577b24af11fa8b00
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-17 22:29:13 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Funksiya Ushbu Baholarni Quyidagi Qoidalarga Kora Harflar Bi Naj0
+Topic: Funksiya ushbu baholarni quyidagi qoidalarga ko‘ra harflar bilan baholab, **yangi ro‘yxat shaklida** natijani  qaytarsin:
+"""
+
+def main():
+    print("Welcome to Funksiya Ushbu Baholarni Quyidagi Qoidalarga Kora Harflar Bi Naj0!")
+    print("Muvaffaqiyatli ishga tushirildi.")
+
+if __name__ == "__main__":
+    main()
